53327ac39c0a486da4be652f1dbce60ed6687bc4
최병민 <89714987+men16922@users.noreply.github.com>
2026-04-06 08:18:23 +0900

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.java
@@ -0,0 +1,34 @@
+class Solution {
+    /*
+        중복된 문자가 없는 가장 긴 부분문자열의 길이 반환
+
+    */
+    public int lengthOfLongestSubstringBruteForce(String s) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            HashSet<Character> charSet = new HashSet<>();
+            for (int j = i; j < s.length(); j++) {
+                if (charSet.contains(s.charAt(j))) break;
+                charSet.add(s.charAt(j));
+            }
+            res = Math.max(res, charSet.size());
+        }
+        return res;
+    }
+
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> charSet = new HashSet<>();
+        int res = 0;
+        int l = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            while (charSet.contains(s.charAt(r))) {
+                charSet.remove(s.charAt(l));
+                l++;
+            }
+            charSet.add(s.charAt(r));
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
