ecbbf20301f5cb4c922f3a21c6376762d81830a4
최병민 <89714987+men16922@users.noreply.github.com>
2026-04-07 02:05:36 +0900

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minBuy = prices[0];
+        int maxProfit = 0;
+        for (int price : prices) {
+            minBuy = Math.min(minBuy, price);
+            maxProfit = Math.max(maxProfit, price - minBuy);
+        }
+        return maxProfit;
+    }
+}
